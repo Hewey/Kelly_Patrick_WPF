cfa8f53b9df2bef025e673124caf64846ecb6324
Hewey <pdkelly@fullsail.edu>
2014-12-03 17:36:38 -0500

    Prompted users name
    Prompted users name

--- GoToTrainingWeek2/js/script.js
+++ GoToTrainingWeek2/js/script.js
@@ -1,1 +1,16 @@
-alert("Sup Brah.");
+/* 
+Patrick Kelly
+SDI Sec 03WWD
+Go To Training Week #2
+12/3/2014
+*/
+
+//alert("Sup Brah.");
+
+//Create an age calculator
+
+//ask the user their name
+
+var name = prompt("Please type in your name.");
+
+alert("Welcome "+name+"! Let's figure out how old you are.");
