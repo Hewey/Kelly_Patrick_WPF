929c3afc610f40e3c968fe5b2a013d4c101fc33b
Hewey <pdkelly@fullsail.edu>
2014-12-04 23:03:51 -0500

    Added my Variables
    Added my Variables

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -6,8 +6,10 @@ Expressions
 
 //alert("Sup brah!");
 
-//Hewey Designs is moving offices and is adding more team members. They need to know how many chairs they need for the new office and how much it will cost.
+//Hewey Designs is moving offices and is adding more team members. They need to know how many chairs they need because they are replacing all the chairs for the new office and how much it will cost. They also need more computers and need to know how much that will cost.
 
 var currentOffice = [10, 4, 5, 1];
-var niceChairs = 150;
-
+var chairCost = 150;
+var newMembers = 6;
+var taxt = 9;
+var computerCost = 1000;
