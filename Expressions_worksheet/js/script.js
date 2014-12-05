f0a0a83aad6332f89193a6132b229d938befe8fc
Hewey <pdkelly@fullsail.edu>
2014-12-04 19:55:00 -0500

    Started Problem 3
    I started problem 3 and declared the variables.

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -18,4 +18,11 @@ var pizzaSlices = 8; //This is the variable representing the ammount of slices o
 var partyPeople = 12; //This the variable for the ammount of people at the party. 
 var pizzaNu = 2; //This is the variable for the ammount of pizzas at the party.
 var slicesPp = pizzaSlices * pizzaNu/partyPeople; //This is the results variable to calculate how many slices of pizza a partygoer gets.
-console.log("Each person ate " + slicesPp + " slices of pizza at the party.");
+console.log("Each person ate " + slicesPp + " slices of pizza at the party."); //This is the results being printed out in the console and integrated into the required sentance.
+
+//At the pizza party Sparky, the host’s dog is excited, because he gets the leftover pizza after the slices have been divided up evenly among the guests. Assume guests get whole slices, how many whole slices will Sparky feast on?
+
+var pizzaSlices = 8;
+var partyPeople = 12;
+var pizzaNu = 2;
+var sparkySlices = 
