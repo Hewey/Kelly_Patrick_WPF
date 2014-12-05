8b25568bea8b7d2c4b1983bea6fe6a845184f697
Hewey <pdkelly@fullsail.edu>
2014-12-04 19:28:22 -0500

    First problem added and commented on
    First problem added and commented on

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -4,4 +4,10 @@ December 4,2014
 Expressions Worksheet
 */
 
-alert("Sup Brah");
+//alert("Sup Brah");
+
+//Calculate how old Sparky the pit bull is in dog years based on his actual age.
+
+var sparkyAge = 1; //This a variable created using the given information of Sparky's age.
+var humanAge= sparkyAge * 7; //This is a result variable using the sparkyAge variable multiplied by 7 to calculate Sparky's age in human years.
+console.log("Sparky is " + humanAge + " human years old which is " + sparkyAge + " in dog years.") //This will print the results in the console. The console should print, "Sparky is 7 human years old which is 1 in dog years."
