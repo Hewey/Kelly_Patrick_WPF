c9ad199e9da7e188a8f1db65e513c401e37e7644
Hewey <pdkelly@fullsail.edu>
2014-12-04 22:38:47 -0500

    Added two arrays
    Added two arrays for common computer resolutions

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -5,3 +5,8 @@ Expressions
 */
 
 //alert("Sup brah!");
+
+//Text box scale calculator
+
+var cresHeight = [1080, 720, 1440]; //cres stand for Computer Resolution.
+var cresWidth =  [1920, 1280, 2560];
