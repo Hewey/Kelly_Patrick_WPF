e78fe4bc84d3fa96c773f626b55993018df8693d
Hewey <pdkelly@fullsail.edu>
2014-12-04 21:52:00 -0500

    I added two variables
    I added two variables to prompt the user for the height and width of
    the wall they would like to build.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -4,4 +4,9 @@ December 4, 2014
 Expressions
 */
 
-alert("Sup Brah!");
+//alert("Sup Brah!");
+
+//Minecraft raw wood to wooden planks calculator for building walls.
+
+var wallHeight = prompt("Please enter the height of the wall you want build.");
+var wallWidth = prompt("Please enter the width of the wall you want to build.")
