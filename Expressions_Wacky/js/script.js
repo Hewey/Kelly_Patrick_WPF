c117213acebead13b03a8a192fffab66a9cb76c4
Hewey <pdkelly@fullsail.edu>
2014-12-04 23:37:02 -0500

    Added variables and tested
    Added variables and tested

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -4,4 +4,11 @@ December 4, 2014
 Expressions
 */
 
-alert("Sup Brah!");
+//alert("Sup Brah!");
+
+//Bob the froggy simulator! When I read wacky this was one of the first things that came to my mind. Don't as why... because I'm not really sure why.
+
+var froggyIntro = prompt("Hey kids welcome to the Bob the Froggy Simulator! \nHere we can simulate the ammount of distance Bob travels depending on the number you enter! Bob travels 5 ft per jump. Enter a number jumps you want Bob to make!");
+var travelFt = froggyIntro * 5;
+var travelMeters = froggyIntro * 5 * (3048/10000);
+console.log(travelMeters);
