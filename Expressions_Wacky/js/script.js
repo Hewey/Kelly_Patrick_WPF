c19241fe77475fc7e76cbac6c12b11f9f59ccc4d
Hewey <pdkelly@fullsail.edu>
2014-12-04 23:43:03 -0500

    Added and tested an alert
    Added and tested an alert

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -6,9 +6,10 @@ Expressions
 
 //alert("Sup Brah!");
 
-//Bob the froggy simulator! When I read wacky this was one of the first things that came to my mind. Don't as why... because I'm not really sure why.
+//Bob the froggy simulator! When I read wacky this was one of the first things that came to my mind. Don't as why... because I'm not really sure.
 
 var froggyIntro = prompt("Hey kids welcome to the Bob the Froggy Simulator! \nHere we can simulate the ammount of distance Bob travels depending on the number you enter! Bob travels 5 ft per jump. Enter a number jumps you want Bob to make!");
 var travelFt = froggyIntro * 5;
-var travelMeters = froggyIntro * 5 * (3048/10000);
-console.log(travelMeters);
+var travelM = froggyIntro * 5 * (3048/10000);
+//console.log(travelM); //tested my meters calculation.
+alert("With " + froggyIntro + " jumps Bob the Froggy traveled " + travelFt + " feet! Converted to meters he traveled " + travelM + " meters! Whew that was quite the trip! Refresh the page to try again!");
