9195d8d0f00d8430e094dbadc39a4dec11de804c
Hewey <pdkelly@fullsail.edu>
2014-12-04 21:57:02 -0500

    Added a variable with an expression
    I added a varaible with an expression and tested to make sure it worked.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -9,4 +9,6 @@ Expressions
 //Minecraft raw wood to wooden planks calculator for building walls.
 
 var wallHeight = prompt("Please enter the height of the wall you want build.");
-var wallWidth = prompt("Please enter the width of the wall you want to build.")
+var wallWidth = prompt("Please enter the width of the wall you want to build.");
+var wallArea = wallHeight * wallWidth;
+//console.log(wallArea); //Here I tested that I correctly saved information from the user and was able to use it in an expression.
