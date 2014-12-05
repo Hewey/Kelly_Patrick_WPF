26845e24f02abc01d7310ee4d2bda5af9d12ad4a
Hewey <pdkelly@fullsail.edu>
2014-12-04 23:50:40 -0500

    fixed spelling errors
    fixed spelling errors

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -8,7 +8,7 @@ Expressions
 
 //Bob the froggy simulator! When I read wacky this was one of the first things that came to my mind. Don't as why... because I'm not really sure.
 
-var froggyIntro = prompt("Hey kids welcome to the Bob the Froggy Simulator! \nHere we can simulate the ammount of distance Bob travels depending on the number you enter! Bob travels 5 ft per jump. Enter a number jumps you want Bob to make!");
+var froggyIntro = prompt("Hey kids welcome to the Bob the Froggy Simulator! \nHere we can simulate the amount of distance Bob travels depending on the number you enter! Bob travels 5 ft per jump. Enter the number jumps you want Bob to make!");
 var travelFt = froggyIntro * 5;
 var travelM = froggyIntro * 5 * (3048/10000);
 //console.log(travelM); //tested my meters calculation.
