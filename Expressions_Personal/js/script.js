508302e1ad4f41252f977b48b9cd1c5ead447d8a
Hewey <pdkelly@fullsail.edu>
2014-12-04 22:15:44 -0500

    Finalized my console.log
    Finalized my console.log

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -13,4 +13,4 @@ var wallWidth = prompt("Please enter the width of the wall you want to build.");
 var wallArea = wallHeight * wallWidth;
 //console.log(wallArea); //Here I tested that I correctly saved information from the user and was able to use it in an expression.
 var rawWood = Math.ceil(wallArea/4);
-console.log("You will need " + wallArea + " wooden planks in total to complete your wall. That turns out to be " + rawWood + " raw wood. With ");
+console.log("You will need " + wallArea + " wooden planks in total to complete your wall. That turns out to be " + rawWood + " raw wood.");
