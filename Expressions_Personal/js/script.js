bfd521cb3576299c958b725574b87d9cc2ef1c6c
Hewey <pdkelly@fullsail.edu>
2014-12-04 22:19:02 -0500

    Added another console.log
    I added another console.log to explain my reasons for making this
    calculator.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -14,3 +14,4 @@ var wallArea = wallHeight * wallWidth;
 //console.log(wallArea); //Here I tested that I correctly saved information from the user and was able to use it in an expression.
 var rawWood = Math.ceil(wallArea/4);
 console.log("You will need " + wallArea + " wooden planks in total to complete your wall. That turns out to be " + rawWood + " raw wood.");
+console.log("I created this because I really enjoy playing minecraft and finding the exact number of blocks for things can sometimes be annyoing. I decided to make this because I know very shortly in one of my projects I'm going to need to build these walls and will need quite a lot of wood to do so. I thought it would be handy to make this to know exaclty how much wood I would need.")
