8843d1456664705e9ca700209538ac4739ec9a4c
Hewey <pdkelly@fullsail.edu>
2014-12-04 19:40:24 -0500

    Expression for problem 2 created and tested
    Expression for problem 2 created and tested

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -11,3 +11,11 @@ Expressions Worksheet
 var sparkyAge = 1; //This a variable created using the given information of Sparky's age.
 var humanAge= sparkyAge * 7; //This is a result variable using the sparkyAge variable multiplied by 7 to calculate Sparky's age in human years.
 console.log("Sparky is " + humanAge + " human years old which is " + sparkyAge + " in dog years.") //This will print the results in the console. The console should print, "Sparky is 7 human years old which is 1 in dog years."
+
+//Create an expression that calculates how much pizza each partygoer will get at the party.
+
+var pizzaSlices = 8; //This is the variable representing the ammount of slices of pizza.
+var partyPeople = 12; //This the variable for the ammount of people at the party. 
+var pizzaNu = 2; //This is the variable for the ammount of pizzas at the party.
+var SlicesPp = pizzaSlices * pizzaNu/partyPeople; //This is the results variable to calculate how many slices of pizza a partygoer gets.
+console.log(SlicesPp);
