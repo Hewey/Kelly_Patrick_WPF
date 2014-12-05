63fa54bc2e7eec7bebfba089850d4b37fd1f13d6
Hewey <pdkelly@fullsail.edu>
2014-12-04 22:41:02 -0500

    Added two more arrays
    I added two more arrays for common mobile arrays.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -10,3 +10,5 @@ Expressions
 
 var cresHeight = [1080, 720, 1440]; //cres stand for Computer Resolution.
 var cresWidth =  [1920, 1280, 2560];
+var mresHeigth = [1920, 1280, 2560]; //mres stands for Mobile Resoltion. 
+var mresWidth = [1080, 720, 1440;]
