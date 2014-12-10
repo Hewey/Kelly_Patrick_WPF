0902659300addd7732ceca39cbfa44acc2318265
Hewey <pdkelly@fullsail.edu>
2014-12-10 17:33:49 -0500

    Tested first fruit in bowl array
    Tested first fruit in bowl array

--- GoToTrainingWeek3/js/week3.js
+++ GoToTrainingWeek3/js/week3.js
@@ -8,3 +8,14 @@ Go To Training Week 3
 //alert("Sup Bruh");
  var bowlOfFruit = ["Apple", "Banana", "Pear", "Peach"];
  console.log(bowlOfFruit[1]);
+
+ console.log(bowlOfFruit.length);
+
+var totalNumPears = 0;
+
+if(bowlOfFruit[0] === "Pear"){
+	console.log("This fruit is a Pear");
+	totalNumPears++;
+} else {
+	console.log("This fruit is not a Pear.");
+}
