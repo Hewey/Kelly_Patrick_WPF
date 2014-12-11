4d81551f8b7e6ea140e94a1111f06090c99aa9d0
Hewey <pdkelly@fullsail.edu>
2014-12-11 18:37:27 -0500

    Added three variables and made some corrections
    Added three variables and made some corrections

--- Conditionals/Conditionals_Wacky/js/script.js
+++ Conditionals/Conditionals_Wacky/js/script.js
@@ -12,12 +12,16 @@ var heroName = prompt("Hello and welcome to the \"Hero Sim\"! Please enter a nam
 if(heroName === ""){
 	console.log("Oh no! Your hero doesn't have a name! Please enter a name for your hero!");
 }else{
-	var heroPower = prompt("Your hero name is " + heroName + ". Now that we have a name for your hero. Let's chose their power! Type one of the following in the box below: Speed, Strength, or Jump.")
+	var heroPower = prompt("Your hero name is " + heroName + ". Now that we have a name for your hero. Let's chose their power! Type one of the following in the box below: Speed, Strength, or Jump.");
 }
 
 if(heroPower != "Strength" || "Speed" || "Jump"){
 	console.log("Oh no! That is not one of the powers! Please try again!")
-	var heroPower = prompt("Your hero name is " + heroName + ". Now that we have a name for your hero. Let's chose their power! Type one of the following in the box below: Speed, Strength, or Jump.")
+	var heroPower = prompt("Your hero name is " + heroName + ". Now that we have a name for your hero. Let's chose their power! Type one of the following in the box below: Speed, Strength, or Jump.");
 }else{
-	var minutes = prompt("You have chosen the " + heroPower " power for your hero! How long in minutes would you like your hero to use thier power?")
+	var minutes = prompt("You have chosen the " + heroPower " power for your hero! How long in minutes would you like your hero to use thier power?");
 }
+
+var powerOne = "Speed";
+var powerTwo = "Strength";
+var powerThree = "Jump";
