a1bb49e9faf8f8612f70382ddf4690fa1b04b169
Hewey <pdkelly@fullsail.edu>
2014-12-11 16:49:03 -0500

    Code added and tested successfully
    Code added and tested successfully

--- Conditionals_worksheet/Group 3/js/script.js
+++ Conditionals_worksheet/Group 3/js/script.js
@@ -5,4 +5,12 @@ Sec 03WDD
 Dec 11, 2014
 */
 
-alert("Sup Bruh");
+//alert("Sup Bruh");
+
+var tirePres = [33, 33, 40, 40];
+//console.log(tirePres);
+if(tirePres[0] === tirePres[1] && tirePres[2] === tirePres[3]){
+	console.log("The tires pass spec!");
+}else{
+	console.log("Get your tires checked out!");
+}
