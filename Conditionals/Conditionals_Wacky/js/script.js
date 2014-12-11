3157578092922baaa1064d387b610d3059796dbe
Hewey <pdkelly@fullsail.edu>
2014-12-11 18:50:00 -0500

    Found an issue am trouble shooting
    Found an issue am trouble shooting

--- Conditionals/Conditionals_Wacky/js/script.js
+++ Conditionals/Conditionals_Wacky/js/script.js
@@ -19,9 +19,15 @@ if(heroPower != "Strength" || "Speed" || "Jump"){
 	console.log("Oh no! That is not one of the powers! Please try again!")
 	var heroPower = prompt("Your hero name is " + heroName + ". Now that we have a name for your hero. Let's chose their power! Type one of the following in the box below: Speed, Strength, or Jump.");
 }else{
-	var minutes = prompt("You have chosen the " + heroPower " power for your hero! How long in minutes would you like your hero to use thier power?");
+	var minutes = prompt("You have chosen the " + heroPower + " power for your hero! How long in minutes would you like your hero to use their power?");
 }
 
 var powerOne = "Speed";
 var powerTwo = "Strength";
 var powerThree = "Jump";
+var powerRate = [400, 12, 4];
+
+if(heroPower === powerOne){
+	var outcomeOne = powerRate[0] * minutes;
+	console.log("Your hero " + heroName + " using speed ran " + outcomeOne + " miles in " + minutes + " at a rate of " + powerRate + " miles a minute. Thank's for playing! To play again please refresh this page.");
+}	
