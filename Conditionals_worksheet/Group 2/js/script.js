9f25bd2340e7271fb51212a35e7c760b43911eb8
Hewey <pdkelly@fullsail.edu>
2014-12-11 16:14:37 -0500

    created some variables
    created some variables

--- Conditionals_worksheet/Group 2/js/script.js
+++ Conditionals_worksheet/Group 2/js/script.js
@@ -6,3 +6,8 @@ Dec 11, 2014
 */
 
 //alert("Sup Brah");
+
+var userName = prompt("Choose a username.");
+var userPass = prompt("Please enter a password.");
+var userName2 = prompt("Please enter your users name.");
+var userPass2 = prompt("Please enter your password.");
