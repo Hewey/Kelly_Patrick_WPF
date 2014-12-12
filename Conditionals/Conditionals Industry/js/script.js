47d0c4d2677305e7a7325e0f0c37d1f465799d81
Hewey <pdkelly@fullsail.edu>
2014-12-11 20:07:48 -0500

    Added two variables and conditions
    Added two variables and conditions and test successfully

--- Conditionals/Conditionals Industry/js/script.js
+++ Conditionals/Conditionals Industry/js/script.js
@@ -4,4 +4,15 @@ December 11,2014
 Conditionals
 */
 
-alert("Sup Bruh!");
+//alert("Sup Bruh!");
+
+var userName = prompt("Please enter a username for your admin account.");
+var passWord = prompt("Please enter a password for your admin account.");
+
+if(userName === ""){
+	console.log("Please enter a username. Do not leave it blank.");
+}
+
+if(passWord === ""){
+	console.log("You need a password. Please enter one!");
+}
