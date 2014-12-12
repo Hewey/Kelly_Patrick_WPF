f6399ab94b1bcd8da03919c21e1ce33ee7a25098
Hewey <pdkelly@fullsail.edu>
2014-12-11 21:45:14 -0500

    Finished the code
    Finished the code

--- Conditionals/Conditionals_Personal/js/script.js
+++ Conditionals/Conditionals_Personal/js/script.js
@@ -9,17 +9,18 @@ Conditionals
 var userName = prompt("Please enter your username.");
 
 if(userName === ""){
-	console.log("Please enter your username don't leave it blank.")
+	console.log("Please enter your username don't leave it blank.");
 }
 var totalShots = prompt("Hello " + userName + " and welcome to the \"How bad is your aim!\" calculator. To start please enter the total number of shots you have made.")
 
 if(isNaN(totalShots)){
-	console.log("Please enter a number! I can't do math with words!")
+	console.log("Please enter a number! I can't do math with words!");
 }
 var totalPer = prompt("Please enter your total percentage of accuracy.");
 
 if(isNaN(totalPer)){
-	console.log("Please enter a number! I can't do math with words!")
+	console.log("Please enter a number! I can't do math with words!");
 }
 
-var totalMiss = totalShots * .totalPer
+var totalHit = totalShots * (totalPer / 100);
+console.log("You have hit a total of " + );
