c610b988b19c59aa34e22e80745b832cbe30acff
Hewey <pdkelly@fullsail.edu>
2014-12-11 20:39:51 -0500

    added more variables and an array
    added more variables and an array

--- Conditionals/Conditionals Industry/js/script.js
+++ Conditionals/Conditionals Industry/js/script.js
@@ -8,6 +8,8 @@ Conditionals
 
 var userName = prompt("Please enter a username for your admin account.");
 var passWord = prompt("Please enter a password for your admin account.");
+var logUser = prompt("Please enter your username.");
+var logPass = prompt("Please enter your password.");
 
 if(userName === ""){
 	console.log("Please enter a username. Do not leave it blank.");
@@ -16,3 +18,16 @@ if(userName === ""){
 if(passWord === ""){
 	console.log("You need a password. Please enter one!");
 }
+
+if(logUser === userName && logPass === passWord){
+	var welcome = prompt("Hello " + userName + "! Welcome to Hewey Design! What would you like to know? If you want to know the number of employees type \"Employee\". If you want to know how many male employees we have type \"Male\". If you want to know how many female employees we have type \"Female\".")
+}else{
+	if(logUser != userName){
+		console.log("Wrong username. Please try again.");
+	}
+	if(logPass != passWord){
+		console.log("Wrong password. Please try again.");
+	}
+}
+var employeesNum = ["male", "female", "male", "male", "female", "male", "male", "female", "female", "male","male", "female", "male", "male", "female", "male", "male", "female", "female", "male","male", "female", "male", "male", "female", "male", "male", "female", "female", "male","male", "female", "male", "male", "female", "male", "male", "female", "female", "male","male", "female", "male", "male", "female", "male", "male", "female", "female", "male","male", "female", "male", "male", "female", "male", "male", "female", "female", "male","male", "female", "male", "male", "female", "male", "male", "female", "female", "male","male", "female", "male", "male", "female", "male", "male", "female", "female", "male","male", "female", "male", "male", "female", "male", "male", "female", "female", "male","male", "female", "male", "male", "female", "male", "male", "female", "female", "male","male", "female", "male", "male", "female", "male", "male", "female", "female", "male","male", "female", "male", "male", "female", "male", "male", "female", "female", "male"]
+console.log(employeesNum.length);
