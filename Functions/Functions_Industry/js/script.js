adb5178f56b29853e816ca2942fb6f5b1aa46e50
Hewey <pdkelly@fullsail.edu>
2014-12-18 19:36:09 -0500

    Started a function and added some arrays
    Started a function and added some arrays

--- Functions/Functions_Industry/js/script.js
+++ Functions/Functions_Industry/js/script.js
@@ -4,4 +4,15 @@ December 18,2014
 Fuctions
 */
 
-alert("Sup Brah");
+//alert("Sup Brah");
+
+//Hewey designs is looking for a new office. They have chosen 3 and need to see if they fit what they are looking for.
+
+var results = function newOffice(){
+	var offficeOne = [400, 300, "suburbs"]
+	var officeTwo = [300, 200, "downtown"]
+	var officeThree = [100, 200, "downtown"]
+	if(){
+		
+	}
+}
