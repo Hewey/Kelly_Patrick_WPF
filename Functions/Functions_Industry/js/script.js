c4b87952d9959a3746181bf37cae96204afb6325
Hewey <pdkelly@fullsail.edu>
2014-12-18 21:05:25 -0500

    Started a condition and added a function
    Started a condition and added a function

--- Functions/Functions_Industry/js/script.js
+++ Functions/Functions_Industry/js/script.js
@@ -8,11 +8,12 @@ Fuctions
 
 //Hewey designs is looking for a new office. They have chosen 3 and need to see if they fit what they are looking for.
 
-var results = function newOffice(){
-	var offficeOne = [400, 300, "suburbs"]
-	var officeTwo = [300, 200, "downtown"]
-	var officeThree = [100, 200, "downtown"]
-	if(){
-		
-	}
+var officeOne = [400, 300, "suburbs", 15000]
+var officeTwo = [300, 200, "downtown" 25000]
+var officeThree = [100, 200, "downtown" 30000]
+if((officeOne[0] * officeOne[1] >= 75000) && (officeOne[2] === "downtown") && (officeOne[3] <= 25000)){
+	function officeOneArea(l, w){
+		var area = l * w;
+		return area;
+	}	
 }
