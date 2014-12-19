867f4d5a421dcf0b29b3943a713772021bef1eeb
Hewey <pdkelly@fullsail.edu>
2014-12-18 19:47:42 -0500

    Added some code and tested
    Added some code and tested. Test failed

--- Functions/Functions_Wacky/js/script.js
+++ Functions/Functions_Wacky/js/script.js
@@ -6,7 +6,9 @@ Fuctions
 
 //alert("Sup Brah");
 
-function pieFace(){
-		
+var welcome = prompt("Welcome to the pie throwing similator. Please enter the first the number of throws you want to make.");
+
+var pieHits = function(welcome){
+	console.log(pieHits);
 }
 
