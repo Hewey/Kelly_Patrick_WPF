1456b51d0b85c9cc8fbd9a36d807c20a4b19832e
Hewey <pdkelly@fullsail.edu>
2014-12-18 21:57:49 -0500

    Added some prompts and tested
    Added some prompts and tested

--- Functions/Functions_Personal/js/script.js
+++ Functions/Functions_Personal/js/script.js
@@ -4,4 +4,9 @@ December 18,2014
 Fuctions
 */
 
-alert("Sup Brah");
+//alert("Sup Brah");
+
+var choiceOne = prompt("Welcome to Random Choice Maker! Maybe you don't know what to eat for lunch. Maybe you don't know what game to play. That's ok! Let Random Choice Maker choose for you! Enter the first item!");
+var choiceTwo = prompt("Enter item two!");
+var choiceThree = prompt("Enter item there!");
+
